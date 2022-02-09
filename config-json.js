c2e40019868191e6c3e86c2aeceb873d88ee3652
Brian Alwill <balwill@salesforce.com>
2022-02-09 17:41:53 -0500

    nulling userInterfaces{}

--- config-json.js
+++ config-json.js
@@ -59,9 +59,7 @@ module.exports = function configJSON(req) {
       }
     },
     userInterfaces: {
-      configModal: {
-        fullscreen: false
-      }
+      
     },
     // schema Object mirrors the activity configuration from the top level of the config.json file and specifies schema information about in and out arguments. Schema objects follow this pattern: 
     // ** 
